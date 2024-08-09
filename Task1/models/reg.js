7cf166e2c69d189c1303fb8b3a7d556ca254dd20
Ankita Nath <145554790+anki2003ta@users.noreply.github.com>
2024-08-10 03:09:15 +0530

    Update reg.js

--- Task1/models/reg.js
+++ Task1/models/reg.js
@@ -11,7 +11,7 @@ main()
     console.log(err);
   });
 async function main() {
- await mongoose.connect("mongodb+srv://Booking:Booking@cluster0.c3z0f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+ await mongoose.connect("mongodb+srv://Booking:Booking@cluster0.c3z0f.mongodb.net/register", {
   bufferCommands: false,
   serverSelectionTimeoutMS: 20000 // increase timeout to 20 seconds
 });
